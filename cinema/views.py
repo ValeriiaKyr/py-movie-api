6b2406b6c062f1ab7fbbc76ec9ffe782044ee94a
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-09-26 17:23:59 +0200

    implement py-movie-api 2

--- cinema/views.py
+++ cinema/views.py
@@ -16,7 +16,7 @@ def movie_list(request: HttpRequest) -> Response:
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -30,7 +30,7 @@ def movie_detail(request: HttpRequest, pk: int) -> Response:
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
